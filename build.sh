6004246f0d11930ca620e7df84fcd88599d12f34
AlexStorm <info@alexstorm.ru>
2023-05-11 16:44:41 +0530

    fix domain

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ helm_build_push(){
   FN=${NAME}-${VER}.tgz
   # rm ${FN}
   helm package ./install --version ${VER}
-  curl --data-binary "@${FN}" https://helm.klogsolenopsys.org/api/charts
+  curl --data-binary "@${FN}" https://helm.solenopsys.org/api/charts
 }
 
-REGISTRY=registry.klogsolenopsys.org
+REGISTRY=registry.solenopsys.org
 NAME=alexstorm-hsm-kubernetes
 ARCHS="linux/amd64,linux/arm64"
 VER=0.1.1
